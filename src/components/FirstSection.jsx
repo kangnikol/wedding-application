8699a248d8ab475e92477dfd5d50718f04345d8a
kangnikol <nicholasalvisaputra@gmail.com>
2023-06-13 14:46:21 +0700

    add favicon, change title, all beginning stuff

---
+++ src/components/FirstSection.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const FirstSection = () => {
+  return (
+    <div className="bg-black h-screen">
+      <h1 className="text-white ">This is First Section</h1>
+    </div>
+  )
+}
+
+export default FirstSection
---
+++ src/components/SecondSection.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const SecondSection = () => {
+  return (
+    <div className="bg-red-500 h-screen">
+      <h1 className="text-white">This is Second Section</h1>
+    </div>
+  )
+}
+
+export default SecondSection
